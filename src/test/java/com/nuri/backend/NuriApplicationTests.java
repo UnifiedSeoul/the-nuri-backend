6ccda438d2597293b61844b9cda0cf7cc1a33364
psg <seunggyu.dev@gmail.com>
2024-04-18 20:07:55 +0900

    first commit

---
+++ src/test/java/com/nuri/backend/NuriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuri.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
