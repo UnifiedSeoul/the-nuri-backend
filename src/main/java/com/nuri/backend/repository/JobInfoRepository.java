182dd1e3597b35fb7a29dc7fbee7de2f19b59b8e
YoonpyoHong <yoon84268426@gmail.com>
2024-04-30 15:26:38 +0900

    feat: pagination

--- src/main/java/com/nuri/backend/repository/JobInfoRepository.java
+++ src/main/java/com/nuri/backend/repository/JobInfoRepository.java
@@ -1,10 +1,16 @@
 package com.nuri.backend.repository;
 
 import com.nuri.backend.domain.JobInfo;
+import com.nuri.backend.dto.JobInfoDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface JobInfoRepository extends JpaRepository<JobInfo, String> {
+    Page<JobInfo> findAll(Pageable pageable);
 
 }
