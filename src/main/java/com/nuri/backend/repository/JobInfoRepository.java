42a54e36f3a67cb7e7b6607085281206eec9154c
psg <seunggyu.dev@gmail.com>
2024-05-05 18:11:47 +0900

    feat: 거리 내에 있는 공고 반환을 위한 native query 개발

--- src/main/java/com/nuri/backend/repository/JobInfoRepository.java
+++ src/main/java/com/nuri/backend/repository/JobInfoRepository.java
@@ -1,10 +1,15 @@
 package com.nuri.backend.repository;
 
 import com.nuri.backend.domain.JobInfo;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface JobInfoRepository extends JpaRepository<JobInfo, String> {
 
+    @Query(value = "SELECT j FROM JobInfo j WHERE distance(j.geoLocation.point, point(:x, :y)) < :distance")
+    List<JobInfo> findJobsWithinDistance(@Param("x") double x, @Param("y") double y, @Param("distance") double distance);
 }
