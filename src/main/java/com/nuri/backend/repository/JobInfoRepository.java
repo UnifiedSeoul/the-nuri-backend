68f1eaeea7dc6e6ca36f3170f87e30db08edae97
psg <seunggyu.dev@gmail.com>
2024-05-07 02:16:10 +0900

    feat: 공고 조회수 증가 시키기, 홈페이지 방문수 증가 시키기 쿼리 구현

--- src/main/java/com/nuri/backend/repository/JobInfoRepository.java
+++ src/main/java/com/nuri/backend/repository/JobInfoRepository.java
@@ -1,19 +1,14 @@
 package com.nuri.backend.repository;
 
 import com.nuri.backend.domain.JobInfo;
-
 import java.util.List;
-
-import com.nuri.backend.dto.JobInfoDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface JobInfoRepository extends JpaRepository<JobInfo, String> {
@@ -21,4 +16,12 @@ public interface JobInfoRepository extends JpaRepository<JobInfo, String> {
 
     @Query(value = "SELECT j FROM JobInfo j WHERE distance(j.geoLocation.point, point(:x, :y)) < :distance")
     List<JobInfo> findJobsWithinDistance(@Param("x") double x, @Param("y") double y, @Param("distance") double distance);
+
+    @Modifying
+    @Query("UPDATE JobInfo j SET j.viewCount = j.viewCount + 1 WHERE j.jobId = :jobId")
+    void incrementViewCount(@Param("jobId") String jobId);
+
+    @Modifying
+    @Query("UPDATE JobInfo j SET j.homePageVisitCount = j.homePageVisitCount + 1 WHERE j.jobId = :jobId")
+    void incrementHomePageViewCount(@Param("jobId") String jobId);
 }
