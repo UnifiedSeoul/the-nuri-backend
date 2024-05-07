e1653ca93134473cb53c3bb48e5e607a423ba563
Yoonpyo Hong <yoon84268426@gmail.com>
2024-05-07 21:44:02 +0900

    fix: application/x-www-urlencoded type ok

--- src/main/java/com/nuri/backend/controller/JoinController.java
+++ src/main/java/com/nuri/backend/controller/JoinController.java
@@ -3,9 +3,7 @@ package com.nuri.backend.controller;
 import com.nuri.backend.dto.api.login.JoinDTO;
 import com.nuri.backend.service.JoinService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @ResponseBody
@@ -17,12 +15,10 @@ public class JoinController {
         this.joinService = joinService;
     }
 
-    @PostMapping("/join")
-    public String joinProcess(@RequestBody JoinDTO joinDTO) {
-
-        System.out.println(joinDTO.getUsername());
+    @PostMapping(value = "/join", consumes = "application/x-www-form-urlencoded")
+    public String joinProcess(@ModelAttribute JoinDTO joinDTO) {
+        System.out.println("Username: " + joinDTO.getUsername());
         joinService.joinProcess(joinDTO);
-
         return "ok";
     }
 }
