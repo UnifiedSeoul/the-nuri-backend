4e9f8b338baff8973b33052b9b42a9bcaf95e824
Yoonpyo Hong <yoon84268426@gmail.com>
2024-05-10 22:23:13 +0900

    feat: login checker

--- src/main/java/com/nuri/backend/controller/JoinController.java
+++ src/main/java/com/nuri/backend/controller/JoinController.java
@@ -1,9 +1,15 @@
 package com.nuri.backend.controller;
 
+import com.nuri.backend.dto.JobInfoDto;
 import com.nuri.backend.dto.api.login.JoinDTO;
 import com.nuri.backend.service.JoinService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -21,5 +27,16 @@ public class JoinController {
         System.out.println("Username: " + joinDTO.getUsername());
         joinService.joinProcess(joinDTO);
         return "ok";
+    }
+
+    @GetMapping("/check")
+    public ResponseEntity<String> loginCheck(Authentication authentication){
+        String username;
+        if(authentication != null && authentication.isAuthenticated()){
+            username = authentication.getName();
+            return ResponseEntity.ok(username);
+        }else {
+            return ResponseEntity.ok("error");
+        }
     }
 }
